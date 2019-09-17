502d079d751429278e99398fc5f3f9d6e79e8cda
Jingjing Li <lijingjing1008@gmail.com>
2019-09-17 22:34:03 +0800

    Download files to destination storage

--- Copy-FileFromAzure.ps1
+++ Copy-FileFromAzure.ps1
@@ -1,6 +1,8 @@
 <# Custom Script for Windows to install a file from Azure Storage using the staging folder created by the deployment script #>
 param (
-    [string]$artifactsFolderName
+    [string]$artifactsFolderName,
+    [string]$desStorageAccountName，
+    [string]$desStorageAccountKey
 )
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name Az -AllowClobber -Force
@@ -8,9 +10,15 @@ $container_name = 'telepathy'
 $destination_path = "C:\telepathy"
 $srcStorageAccountName = "soaserviceartifactci"
 $srcStorageAccountKey = "CIUUcB0qMYYZAj+Zz9zllWX8D2WacMdm6uHRr0oqzuLYnaFbmZkY+GDdmIoWETkDYfJhpK1V6ltVrwOq5u13sw=="
-$storage_account = New-AzStorageContext -StorageAccountName $srcStorageAccountName -StorageAccountKey $srcStorageAccountKey
+$desStorageAccountName = ""
+$desStorageAccountKey = ""
+$srcStorageContext = New-AzStorageContext -StorageAccountName $srcStorageAccountName -StorageAccountKey $srcStorageAccountKey
+$desStorageContext = New-AzStorageContext -StorageAccountName $destorageAccountName -StorageAccountKey $desStorageAccountKey
 $blobs = Get-AzStorageBlob -Container $container_name -Blob $artifactsFolderName -Context $storage_account  
 foreach($blob in $blobs) {  
     New-Item -ItemType Directory -Force -Path $destination_path  
-    Get-AzStorageBlobContent -Container $container_name -Blob $blob.Name -Destination $destination_path -Context $storage_account  
+    Get-AzStorageBlobContent -Container $container_name -Blob $blob.Name -Destination $destination_path -Context $srcStorageContext   
+    Set-AzStorageBlobContent -Container $container_name -Blob $blob.Name -Context $desStorageContext
 } 
+
+
