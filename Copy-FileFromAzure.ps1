fafb00957afa2908bbe92631c60012bbe6a812ab
Jingjing Li <lijingjing1008@gmail.com>
2019-09-19 17:03:06 +0800

    Seperate start script logic into start session launcher and enable telepathy storage

--- Copy-FileFromAzure.ps1
+++ Copy-FileFromAzure.ps1
@@ -1,5 +1,7 @@
 <# Custom Script for Windows to install a file from Azure Storage using the staging folder created by the deployment script #>
 param (
+    [switch]$EnableTelepathyStorage,
+    [switch]$StartSessionLauncher,
     [string]$Location,
     [string]$BatchAccountName,
     [string]$BatchPoolName,
@@ -15,45 +17,19 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name Az -AllowClobber -Force
 $destination_path = "C:\telepathy"
 $srcStorageContext = New-AzStorageContext -StorageAccountName $srcStorageAccountName -StorageAccountKey $srcStorageAccountKey
-$desStorageContext = New-AzStorageContext -StorageAccountName $desStorageAccountName -StorageAccountKey $desStorageAccountKey
 $blobs = Get-AzStorageBlob -Container $containerName -Blob "$artifactsFolderName*" -Context $srcStorageContext
 foreach($blob in $blobs) {  
     New-Item -ItemType Directory -Force -Path $destination_path  
     Get-AzStorageBlobContent -Container $containerName -Blob $blob.Name -Destination $destination_path -Context $srcStorageContext   
 } 
 
-"runtime service-assembly service-registration".split() | New-AzStorageContainer -Context $desStorageContext
-
-function uploadFiles{
-    param($LocalPath, $RemotePath, $ContainerName, $StorageContext)
-    $files = Get-childItem $localPath
-    foreach($file in $files)
-    {
-        $localFile = "$localPath\$file"
-        if($RemotePath) {
-            $remoteFile= "$remotePath/$file"
-        }
-        else {
-            $remoteFile= "$file"
-        }
-        
-        if(-not $file.PSIsContainer) {
-            Set-AzStorageBlobContent -File $localFile -Blob $remoteFile -Container $ContainerName -Context $StorageContext -Force
-        }
-        else {
-            uploadFiles -LocalPath $localFile -RemotePath $remoteFile -ContainerName $containerName -StorageContext $storageContext 
-        }
-        
-    }
-}
-
-uploadFiles -LocalPath "$destination_path\$artifactsFolderName\Release\CcpServiceHost" -RemotePath "ccpservicehost" -ContainerName "runtime" -StorageContext $desStorageContext
-uploadFiles -LocalPath "$destination_path\$artifactsFolderName\Release\EchoSvcLib" -RemotePath "ccpechosvc" -ContainerName "service-assembly" -StorageContext $desStorageContext
-uploadFiles -LocalPath "$destination_path\$artifactsFolderName\Release\Registration"  -ContainerName "service-registration" -StorageContext $desStorageContext
-
+$artifactsPath = "$destination_path\$artifactsFolderName\Release"
 $desStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=$desStorageAccountName;AccountKey=$desStorageAccountKey;EndpointSuffix=core.windows.net"
 $batchServiceUrl = "https://$BatchAccountName.$location.batch.azure.com"
+if($EnableTelepathyStorage) {
+    invoke-expression "$artifactsPath\EnableTelepathyStorage.ps1 -DestinationPath $artifactsPath -DesStorageConnectionString '$desStorageConnectionString'"
+}
 
- Write-Verbose $desStorageConnectionString
- Write-Verbose $BatchAccountKey
- Write-Verbose $batchServiceUrl
+if($StartSessionLauncher) {
+    invoke-expression "$artifactsPath\StartSessionLauncher.ps1 -DestinationPath $artifactsPath -DesStorageConnectionString '$desStorageConnectionString' -BatchAccountName $BatchAccountName -BatchPoolName $BatchPoolName -BatchAccountKey $BatchAccountKey -BatchAccountServiceUrl $batchServiceUrl"
+}
