076847528161a219fad435e52346032a8e0a752c
Jingjing Li <lijingjing1008@gmail.com>
2019-09-23 17:29:04 +0800

    Write log to file.

--- StartTelepathy.ps1
+++ StartTelepathy.ps1
@@ -13,36 +13,109 @@ param (
     [string]$DesStorageAccountKey,
     [string]$BatchAccountKey
 )
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Install-Module -Name Az -AllowClobber -Force
+
+function Write-Log 
+{ 
+    [CmdletBinding()] 
+    Param 
+    ( 
+        [Parameter(Mandatory=$true, 
+                   ValueFromPipelineByPropertyName=$true)] 
+        [ValidateNotNullOrEmpty()] 
+        [Alias("LogContent")] 
+        [string]$Message, 
+ 
+        [Parameter(Mandatory=$false)] 
+        [Alias('LogPath')] 
+        [string]$Path='C:\Logs\telepathy.log', 
+         
+        [Parameter(Mandatory=$false)] 
+        [ValidateSet("Error","Warn","Info")] 
+        [string]$Level="Info", 
+         
+        [Parameter(Mandatory=$false)] 
+        [switch]$NoClobber 
+    ) 
+ 
+    Begin 
+    { 
+        # Set VerbosePreference to Continue so that verbose messages are displayed. 
+        $VerbosePreference = 'Continue' 
+    } 
+    Process 
+    { 
+         
+        # If the file already exists and NoClobber was specified, do not write to the log. 
+        if ((Test-Path $Path) -AND $NoClobber) { 
+            Write-Error "Log file $Path already exists, and you specified NoClobber. Either delete the file or specify a different name." 
+            Return 
+            } 
+ 
+        # If attempting to write to a log file in a folder/path that doesn't exist create the file including the path. 
+        elseif (!(Test-Path $Path)) { 
+            Write-Verbose "Creating $Path." 
+            $NewLogFile = New-Item $Path -Force -ItemType File 
+            } 
+ 
+        else { 
+            # Nothing to see here yet. 
+            } 
+ 
+        # Format Date for our Log File 
+        $FormattedDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss" 
+ 
+        # Write message to error, warning, or verbose pipeline and specify $LevelText 
+        switch ($Level) { 
+            'Error' { 
+                Write-Error $Message 
+                $LevelText = 'ERROR:' 
+                } 
+            'Warn' { 
+                Write-Warning $Message 
+                $LevelText = 'WARNING:' 
+                } 
+            'Info' { 
+                Write-Verbose $Message 
+                $LevelText = 'INFO:' 
+                } 
+            } 
+         
+        # Write log entry to $Path 
+        "$FormattedDate $LevelText $Message" | Out-File -FilePath $Path -Append 
+    } 
+    End 
+    { 
+    } 
+}
+ 
 $destination_path = "C:\telepathy"
 Try {
-    $srcStorageContext = New-AzStorageContext -StorageAccountName $SrcStorageAccountName -SasToken $SrcStorageContainerSasToken
-    Write-Host "StorageAccountName : $SrcStorageAccountName"
-    Write-Host "StorageSasToken : $SrcStorageContainerSasToken"
+    Write-Log -Message "StorageAccountName : $SrcStorageAccountName"
+    Write-Log -Message "StorageSasToken : $SrcStorageContainerSasToken"
+    $srcStorageContext = New-AzStorageContext -StorageAccountName $SrcStorageAccountName -SasToken $SrcStorageContainerSasToken  
 } Catch {
-    Write-Host "Please provide valid storage account name and sas token"
-    Write-Host $_
+    Write-Log -Message "Please provide valid storage account name and sas token"
+    Write-Log -Message $_
 }
 
 Try {
-    Write-Host "ContainerName : $ContainerName"
-    Write-Host "ArtifactsFolderName : $ArtifactsFolderName"
+    Write-Log -Message "ContainerName : $ContainerName"
+    Write-Log -Message "ArtifactsFolderName : $ArtifactsFolderName"
     $blobs = Get-AzStorageBlob -Container $ContainerName -Blob "$ArtifactsFolderName*" -Context $srcStorageContext
 } Catch {
-    Write-Host "Error occurs when get source storage blob, can't get storage blob, please confirm you provide valid container name, blob name and storage context "
-    Write-Host $_
+    Write-Log -Message "Error occurs when get source storage blob, can't get storage blob, please confirm you provide valid container name, blob name and storage context "
+    Write-Log -Message $_
 }
 
 Try {
-    Write-Host "DestinationPath in VM : $destination_path"
+    Write-Log -Message "DestinationPath in VM : $destination_path"
     foreach($blob in $blobs) {  
         New-Item -ItemType Directory -Force -Path $destination_path  
         Get-AzStorageBlobContent -Container $ContainerName -Blob $blob.Name -Destination $destination_path -Context $srcStorageContext   
     } 
 } Catch {
-    Write-Host "Error occurs when download source storage blob to VM "
-    Write-Host $_
+    Write-Log -Message "Error occurs when download source storage blob to VM "
+    Write-Log -Message $_
 }
 
 $artifactsPath = "$destination_path\$ArtifactsFolderName\Release"
